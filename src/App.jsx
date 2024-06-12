44e4178f85dfe5f629877bc8a13224406d8cd694
harimbu <harimbu@gmail.com>
2024-06-12 11:51:19 +0900

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,22 @@
+import { useState } from 'react'
 import './App.css'
 
 export default function App() {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      title: '청소하기',
+    },
+    {
+      id: 2,
+      title: '일찍 일어나기',
+    },
+    {
+      id: 3,
+      title: '자바스크립트 공부하기',
+    },
+  ])
+
   return (
     <div className='container'>
       <div className='header'>
@@ -13,27 +29,15 @@ export default function App() {
       </form>
 
       <div className='todos'>
-        <div className='todo_item'>
-          <input type='checkbox' name='' id='' /> 안녕하세요
-          <div className='btns'>
-            <button>수정</button>
-            <button>삭제</button>
+        {todos.map((todo) => (
+          <div className='todo_item'>
+            <input type='checkbox' name='' id='' /> 안녕하세요
+            <div className='btns'>
+              <button>수정</button>
+              <button>삭제</button>
+            </div>
           </div>
-        </div>
-        <div className='todo_item'>
-          <input type='checkbox' name='' id='' /> 안녕하세요
-          <div className='btns'>
-            <button>수정</button>
-            <button>삭제</button>
-          </div>
-        </div>
-        <div className='todo_item'>
-          <input type='checkbox' name='' id='' /> 안녕하세요
-          <div className='btns'>
-            <button>수정</button>
-            <button>삭제</button>
-          </div>
-        </div>
+        ))}
       </div>
 
       <div className='todo_bottom'>
