2e93be0364d24d5c3f5fa3253be2a6689942b8ad
harimbu <harimbu@gmail.com>
2024-06-13 09:50:27 +0900

    init

--- src/components/TodoInput.jsx
+++ src/components/TodoInput.jsx
@@ -1,7 +1,23 @@
+import { collection, doc, setDoc } from 'firebase/firestore'
+import { useState } from 'react'
+
 export default function TodoInput() {
+  const [title, setTitle] = useState('안녕')
+
+  function handleSubmit(e) {
+    e.preventDefault()
+
+    console.log('abc')
+  }
+
   return (
-    <form>
-      <input type='text' placeholder='Write a todo..' />
+    <form onSubmit={handleSubmit}>
+      <input
+        type='text'
+        placeholder='Write a todo..'
+        value={title}
+        onChange={() => {}}
+      />
     </form>
   )
 }
